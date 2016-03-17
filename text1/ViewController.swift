b800e0ba207e15dee96decccf60e789d0012b4f1
lvguorui <lvguorui@admindeiMac-33.local>
2016-03-17 11:51:01 +0800

    add
    add

---
+++ text1/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  text1
+//
+//  Created by lvguorui on 16/3/17.
+//  Copyright (c) 2016年 lvguorui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var x: UITextField!
+    @IBOutlet weak var y: UITextField!
+    @IBOutlet weak var z: UITextField!
+    @IBAction func add(sender: AnyObject) {
+        var a:Double!=0
+        var b:Double!=0
+        var c:Double!=0
+        if(!x.text.isEmpty){
+            a=(x.text! as NSString).doubleValue
+        }
+        if(!y.text.isEmpty){
+            b=(y.text! as NSString).doubleValue
+        }
+        c=a+b
+        z.text="\(c)"
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+   
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
